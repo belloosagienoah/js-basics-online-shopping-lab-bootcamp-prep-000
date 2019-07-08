3c0eeb5738c7876ba02a02e9177b515e5cae43f3
Bello Osagie <osagienoah04@gmail.com>
2019-07-08 20:28:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,63 +10,21 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var newItem = {[item]: Math.floor( Math.random(1, 101) * 100 ) };
- cart.push(newItem);
- console.log(`${item} has been added to your cart.`);
- return cart;
+ // write your code here
 }
 
 function viewCart() {
-  if (!cart.length) {
-    return console.log("Your shopping cart is empty.")
-  }
-
-  const itemsWithPrices = [];
-
-  for (let i = 0; i < cart.length; i++) {
-    var itemNames = Object.keys(cart[i]);
-    var itemName = itemNames[0];
-    var itemPrice = cart[i][itemName]
-
-    itemsWithPrices.push(`${itemName} at $${itemPrice}`)
-  }
-
-  if (itemsWithPrices.length === 1) {
-    return console.log(`In your cart, you have ${itemsWithPrices.join()}.`)
-  } else if (itemsWithPrices.length === 2) {
-    return console.log(`In your cart, you have ${itemsWithPrices[0]} and ${itemsWithPrices.slice(-1)}.`)
-  } else {
-    return console.log(`In your cart, you have ${itemsWithPrices.slice(0,-1).join(', ')}, and ${itemsWithPrices.slice(-1)}.`)
-  }
-
+  // write your code here
 }
 
 function total() {
-  var total = 0;
-  for (let i = 0; i < cart.length; i++) {
-    var itemNames = Object.keys(cart[i]);
-    var itemName = itemNames[0];
-    total += parseInt(cart[i][itemName], 10);
-  }
-  return total;
+  // write your code here
 }
 
-function removeFromCart(itemName) {
-  for (let i = 0; i < cart.length; i++) {
-    if (cart[i].hasOwnProperty(itemName)) {
-      return cart.splice(i, 1);
-    }
-  }
-
-  return console.log("That item is not in your cart.");
+function removeFromCart(item) {
+  // write your code here
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber) {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
-    cart = [];
-    return cart;
-  } else {
-    return console.log("Sorry, we don't have a credit card on file for you.");
-  }
+  // write your code here
 }
